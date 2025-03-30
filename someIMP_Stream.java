14a815d4cc8ec90dd34cbcea420a9ae8e52d7139
yvyaswanth13 <84392943+yvyaswanth13@users.noreply.github.com>
2025-03-31 00:06:29 +0530

    Create someIMP_Stream.java

---
+++ someIMP_Stream.java
@@ -0,0 +1,24 @@
+List<Employee> maxSalary = employees.stream()
+    .filter(e -> e.salary > 5000)
+    .collect(Collectors.toList());
+Max salary:
+Optional<Employee> maxSalaryEmployee = employees.stream()
+    .max(Comparator.comparingDouble(e -> e.salary));
+
+Note: List Might be empty so Optional
+
+List<Employee> sortedEmployees = employees.stream()
+    .sorted((e1, e2) -> Integer.compare(e1.age, e2.age)) // Sort by age in ascending order
+    .collect(Collectors.toList())
+Note: Streams wont use sort it will use sorted
+normal java expression:students.sort((p1, p2) -> Integer.compare(p1.age, p2.age));
+
+//Optimized way:
+Decending order:
+List<Employee> sortedEmployees = employees.stream()
+    .sorted(Comparator.comparingInt(e -> e.age).reversed())
+    .collect(Collectors.toList());
+
+List<Employee> sortedEmployees = employees.stream()
+    .sorted(Comparator.comparingInt(e -> e.age))
+    .collect(Collectors.toList());
